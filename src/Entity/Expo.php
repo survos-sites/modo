df8289d11ccb29a630a0677b7beddef5121460c2
Tac Tacelosky <tacman@gmail.com>
2025-04-06 09:14:38 -0400

    add locale to api route

--- src/Entity/Expo.php
+++ src/Entity/Expo.php
@@ -2,12 +2,23 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ExpoRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
 use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: ExpoRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['expo.read']],
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ]
+)]
 class Expo implements TranslatableInterface
 {
     use TranslatableTrait;
@@ -16,6 +27,7 @@ class Expo implements TranslatableInterface
     public function __construct(
         #[ORM\Id]
         #[ORM\Column(length: 32)]
+        #[Groups(['expo.read'])]
         private ?string $code = null
     )
     {
@@ -27,10 +39,12 @@ class Expo implements TranslatableInterface
         return $this->code;
     }
 
+    #[Groups(['expo.read'])]
     public function getTitle(): string
     {
         return $this->translate()->getTitle();
     }
+    #[Groups(['expo.read'])]
     public function getContent(): string
     {
         return $this->translate()->getContent();
